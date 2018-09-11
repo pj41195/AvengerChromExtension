6cdf77c359e123fa81cd51130c1dacda0ef5627c
Praval Jain <praval41195jain@gmail.com>
2018-09-11 16:49:41 +0530

    commitExtension

---
+++ js/main.js
@@ -0,0 +1,46 @@
+var images = [
+"https://hdwallsource.com/img/2014/7/dc-comics-wallpaper-22345-22955-hd-wallpapers.jpg",
+"https://www.hdwallpapersfreedownload.com/uploads/large/super-heroes/marvel-avengers-hd-background-wallpaper.jpg",
+"https://wallpapercave.com/wp/wp2577612.jpg",
+"https://wallpapertag.com/wallpaper/full/3/2/a/49099-beautiful-avengers-wallpaper-2560x1600-mobile.jpg",
+"https://i.imgur.com/Hykr7by.jpg",
+"https://wallpaper-house.com/data/out/9/wallpaper2you_341863.jpg",
+"https://vignette.wikia.nocookie.net/marvel/images/c/c3/Hulk-los-vengadores-1280x1024.jpg/revision/latest?cb=20130302205826&path-prefix=es",
+"http://levesqueforcongress.com/wp-content/uploads/2018/07/thor-hd-wallpapers-awesome-the-avengers-loki-hd-desktop-wallpaper-high-definition-mobile-of-thor-hd-wallpapers.jpg",
+"https://www.comicbookmovie.com/images/wallpaper/255_877.jpg",
+"http://papers.co/wallpaper/papers.co-be77-spiderman-hero-marvel-avengers-art-illustration-29-wallpaper.jpg",
+"https://i.pinimg.com/originals/9e/75/54/9e7554392d4cb18b1843fbd719cc9abe.jpg",
+"http://papers.co/wallpaper/papers.co-be85-avengers-danai-girl-blackpanther-hero-marvel-art-film-29-wallpaper.jpg",
+"http://www.hdwallpaperslife.com/wp-content/uploads/2017/10/doctor_strange_minimal-HD.jpg",
+"https://images.sftcdn.net/images/t_app-cover-l,f_auto/p/93d7c950-9b29-11e6-92eb-00163ec9f5fa/3845669813/marvels-the-avengers-wallpaper-screenshot.png",
+"https://banner2.kisspng.com/20180325/yze/kisspng-captain-america-quicksilver-thor-black-widow-deskt-avengers-5ab807379dd052.9034270715220099116464.jpg",
+"http://www.wallpapers13.com/wp-content/uploads/2017/03/Avengers-Scarlett-Johansson-Black-Widow-Full-Hd-Wallpapers-For-Desktop-1920x1200-915x515.jpg",
+"http://www.wallpapers13.com/wp-content/uploads/2017/03/Avengers-Scarlett-Johansson-Black-Widow-Full-Hd-Wallpapers-For-Desktop-1920x1200-915x515.jpg",
+"https://www.hdwallpapersfreedownload.com/uploads/large/super-heroes/iron-man-wallpaper-fantastic-full-hd.jpg",
+"https://amazingpict.com/wp-content/uploads/2015/04/avengers-assemble.jpg",
+"https://wallpaper-house.com/data/out/5/wallpaper2you_65800.jpg",
+"https://wallpaper-house.com/data/out/12/wallpaper2you_546506.jpg",
+"http://kb4images.com/images/dc-comics-wallpapers/38131676-dc-comics-wallpapers.jpg",
+"https://s3-imgs.niusnews.com/130711_0.jpg",
+"http://www.geniales.info/wp-content/uploads/2018/08/charming-injustice-2-wallpaper-and-image-batman-0001-jpg-dc-database-fandom.jpg",
+"https://cdn.wallpapersafari.com/16/35/n980LB.jpg",
+"http://getwallpapers.com/wallpaper/full/3/0/e/332560.jpg",
+"http://3.bp.blogspot.com/-1sagSURcCCM/UlfvPUzNCII/AAAAAAAAAIQ/gJfWCVkt_6M/s1600/batman-origens.jpg",
+"https://www.wallpapersin4k.org/wp-content/uploads/2017/04/Best-Dc-Wallpapers-12.jpg",
+"https://pre00.deviantart.net/c44b/th/pre/f/2014/007/7/e/iron_man_in_black_by_deviationanonymous-d716753.png",
+"https://wallpaper-house.com/data/out/5/wallpaper2you_64566.jpg"
+
+
+
+]
+
+
+
+
+window.onload = codeAddress;
+
+function codeAddress() {
+	 var x = Math.floor((Math.random() * 29) + 0)
+	var t=document.getElementById("image");
+	t.src=images[x];
+}
